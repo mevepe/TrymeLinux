e4efce97e52fa9472b53b34bd12a7dc717813691
mevepe <udestroyed@gmail.com>
2026-03-13 01:49:16 +0300

    Cross compile with gpiod passing

--- TrymeLinux/TrymeLinux.cpp
+++ TrymeLinux/TrymeLinux.cpp
@@ -1,8 +1,32 @@
-﻿#include "TrymeLinux.h"
+﻿#include <gpiod.hpp>
+#include <iostream>
+#include <thread>
+#include <chrono>
 
 int main()
 {
 	std::cout << "Hello CMake.\n";
+
+	const char* chipname = "/dev/gpiochip0";
+	int line_num = 15; // PA15
+
+	try {
+		gpiod::chip chip(chipname);
+		gpiod::line line = chip.get_line(line_num);
+
+		line.request({ "gpio-example", gpiod::line_request::DIRECTION_OUTPUT, 0 }, 0);
+
+		for (int i = 0; i < 10; i++) {
+			line.set_value(1);
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+			line.set_value(0);
+			std::this_thread::sleep_for(std::chrono::milliseconds(500));
+		}
+
+	}
+	catch (const std::exception& e) {
+		std::cerr << "Ошибка: " << e.what() << std::endl;
+	}
 
 	return 0;
 }
--- cmake/toolchains/aarch64-linux-gnu.cmake
+++ cmake/toolchains/aarch64-linux-gnu.cmake
@@ -2,15 +2,26 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
-# Adjust the path to your installed cross compilers in WSL (example for Ubuntu)
-# Common installation places compilers under /usr/bin, so set CROSS_ROOT to /usr
-set(CROSS_ROOT /usr)
-set(CMAKE_C_COMPILER ${CROSS_ROOT}/bin/aarch64-linux-gnu-gcc)
-set(CMAKE_CXX_COMPILER ${CROSS_ROOT}/bin/aarch64-linux-gnu-g++)
-set(CMAKE_FIND_ROOT_PATH ${CROSS_ROOT})
+# Путь к rootfs Armbian (ОБЯЗАТЕЛЬНО укажи свой путь!)
+set(CMAKE_SYSROOT "/home/me/armbian-rootfs")
 
-# Search for programs in the build host directories
+# Кросс-компиляторы
+set(CMAKE_C_COMPILER   /usr/bin/aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
+
+# Настройка sysroot для компилятора и линковщика
+set(CMAKE_C_FLAGS          "--sysroot=${CMAKE_SYSROOT}")
+set(CMAKE_CXX_FLAGS        "--sysroot=${CMAKE_SYSROOT}")
+set(CMAKE_EXE_LINKER_FLAGS "--sysroot=${CMAKE_SYSROOT}")
+
+# Пути поиска библиотек и заголовков — только внутри rootfs
+set(CMAKE_FIND_ROOT_PATH
+    ${CMAKE_SYSROOT}
+)
+
+# Программы ищем в системе WSL
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
-# For libraries and headers in the target directories
+
+# Библиотеки и заголовки — только в rootfs
 set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
 set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
