2b3d326babc6016280e13c7ea3e93c68df9fada7
mevepe <udestroyed@gmail.com>
2026-03-13 17:26:33 +0300

    Finally

--- cmake/toolchains/aarch64-linux-gnu.cmake
+++ cmake/toolchains/aarch64-linux-gnu.cmake
@@ -2,23 +2,31 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR aarch64)
 
+set(CMAKE_SYSROOT "${ROOTFS}/usr")
+
 # Кросс-компиляторы
 set(CMAKE_C_COMPILER   /usr/bin/aarch64-linux-gnu-gcc)
 set(CMAKE_CXX_COMPILER /usr/bin/aarch64-linux-gnu-g++)
 
 # Настройка sysroot для компилятора и линковщика
-#set(CMAKE_C_FLAGS          "--sysroot=${ROOTFS}")
-#set(CMAKE_CXX_FLAGS        "--sysroot=${ROOTFS}")
-#set(CMAKE_EXE_LINKER_FLAGS "--sysroot=${ROOTFS}")
+set(CMAKE_C_FLAGS          "--sysroot=${CMAKE_SYSROOT}")
+set(CMAKE_CXX_FLAGS        "--sysroot=${CMAKE_SYSROOT}")
+set(CMAKE_EXE_LINKER_FLAGS "--sysroot=${CMAKE_SYSROOT}")
 
 # Пути поиска библиотек и заголовков — только внутри rootfs
-#set(CMAKE_FIND_ROOT_PATH
-#    ${ROOTFS}
-#)
+set(CMAKE_FIND_ROOT_PATH
+    ${CMAKE_SYSROOT}
+)
+
+# Указываем sysroot для компилятора и линковщика
+set(CMAKE_SYSROOT_COMPILE ${CMAKE_SYSROOT})
+set(CMAKE_SYSROOT_LINK    ${CMAKE_SYSROOT})
 
 # Программы ищем в системе WSL
-#set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
 
 # Библиотеки и заголовки — только в rootfs
-#set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
-#set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
+
